9f9e4f858b5c009c7f3f634e6fdf60013bc2ca6f
Mitchell <soggybag@gmail.com>
2021-02-23 16:54:21 -0800

    Initial Commit

---
+++ MatchGame/MatchGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchGameApp.swift
+//  MatchGame
+//
+//  Created by Mitchell Hudson on 2/23/21.
+//
+
+import SwiftUI
+
+@main
+struct MatchGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
